11d8ea4c92aa7d6f373bd6b2751d4804f35f608b
sarathchandrika <sarath182015@gmail.com>
2016-11-05 18:50:22 +0530

    Code added

---
+++ joomla/Joomla_3.6.0-Stable-Full_Package/administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-10-06 08:24:38 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-10-06T08:24:38+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-10-06T08:24:54+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-10-06T08:32:43+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
